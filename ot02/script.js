6ac82b70dff809c51bfd8ed38e08a56b15803784
IanDerick <ian.derick@outlook.com>
2025-05-14 20:09:56 -0300

    criando algumas Diretivas com VueJS

---
+++ ot02/script.js
@@ -0,0 +1,38 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        url: 'https://vuejs.org'
+    }
+});
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: ''
+    }
+});
+
+var app3 = new Vue({
+    el: '#app3',
+    data:{
+        seen: true
+    }
+});
+
+var app4 = new Vue({
+    el: '#app4',
+    data: {
+        status: true,
+        titulo: 'Aprendendo Vue Js'
+    }
+});
+
+var app5 = new Vue({
+    el: '#app5',
+    data:{
+        id: 1,
+        nome: 'Ian Derick',
+        email: 'ian_mota@gmail.com',
+        profissao: 'Analista de Suporte Jr.'
+    }
+});
