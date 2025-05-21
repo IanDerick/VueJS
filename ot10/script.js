542dd336f75629256bd8443d61a322efd9e96eb2
IanDerick <ian.derick@outlook.com>
2025-05-21 19:28:24 -0300

    Finalizando a explicação de componentes reutilizaveis

--- ot10/script.js
+++ ot10/script.js
@@ -15,18 +15,29 @@ new Vue({
             alert('O botão foi clicado!')
         }
     }
-});*/
+});
 
 Vue.component('component-a', {
-    template: '<div>Este é o Componente A</div>'
-});
-Vue.component('component-b', {
-    template: '<div>Este é o Componente B</div>'
-});
-
-new Vue({
-    el:'#app2',
+    template: '<div><h2>Este é o Componente A</h2></div>'
+  });
+  
+  Vue.component('component-b', {
+    template: '<div><h2>Este é o Componente B</h2></div>'
+  });
+  
+  new Vue({
+    el: '#app',
     data: {
-        currentComponent: 'component-a'
+      currentComponent: 'component-a'
     }
-})
+  });*/
+
+Vue.component('menu-bar', {
+    template: "#menu"
+});
+var app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Vue Js',
+    }
+});
